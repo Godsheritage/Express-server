d275f14969df4fd910e56670aae0ad644f47d301
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-09 18:20:31 +0100

    parameterized routing with error handling

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
-const express = require('express')
+const express = require("express");
 
 const server = express();
 
-const PORT = 3000
+const PORT = 3000;
 
-server.get('/', (req, res) => {
-    res.send('welcome to your very first Express API')
-})
-server.get('/greeting', (req, res) => {
-    res.send('awfa my guy, how you dey')
-})
-server.get('/salute', (req, res) => {
-    res.send('boohoohahaha')
-})
+const friends = [
+  {
+    id: 0,
+    name: "Godsheritage Adeoye",
+  },
+  {
+    id: 1,
+    name: "Crownfit Adeoye",
+  },
+  {
+    id: 2,
+    name: "Mojola Adeoye",
+  },
+];
+
+server.get("/", (req, res) => {
+  res.send("awfa my guy, how you dey");
+});
+
+//to create friend route
+
+server.get("/friends", (req, res) => {
+  res.send(friends);
+});
+
+// for parameterized routing with error handling
+
+server.get("/friends/:id", (req, res) => {
+  const id = +req.params.id;
+  const friend = friends[id];
+
+  if (friend) {
+    res.send(friend);
+  } else {
+    res.status(404).json({
+      error: "not found",
+    });
+  }
+});
 
 server.listen(PORT, () => {
-    console.log('Express server is running ...')
-})
+  console.log("Express server is running ...");
+});

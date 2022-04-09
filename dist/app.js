20b070832b3349e5d06dd575cc1e9dbcd59a5228
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-09 17:24:07 +0100

    updates

--- dist/app.js
+++ dist/app.js
@@ -4,14 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-app.use(express_1.default.static(path_1.default.join(__dirname, "..", "public")));
+app.use((0, helmet_1.default)());
 app.get("/secret", (req, res) => {
     return res.json({
         secretNumber: "Your secret number is 49",
     });
 });
+app.use(express_1.default.static(path_1.default.join(__dirname, "..", "public")));
 app.get("/", (req, res) => {
     res.sendFile(path_1.default.join(__dirname, "..", "public", "index.html"));
 });
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express from "express";
+import helmet from "helmet";
 import path from "path";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(helmet())
 
 app.get("/secret", (req, res) => {
   return res.json({
     secretNumber: "Your secret number is 49",
   });
 });
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));

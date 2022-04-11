1bc2b4978fe94a812ea85fb015b056f533273768
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-11 13:39:18 +0100

    updates

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,26 @@ dotenv.config();
 
 const app = express();
 
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRETS: process.env.CLIENT_SECRETS,
+};
+
+const AUTH_OPTIONS = {
+  callBackURL: "auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRETS,
+};
+
+const verifyCallback: any = (accessToken:any, refreshToken: any, profile:any, done:any) => {
+  console.log(`Google Profile ${profile}`);
+  done(null, profile);
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 app.use(helmet());
+app.use(passport.initialize());
 
 const checkLoggedIn: RequestHandler = (req, res, next) => {
   const isLoggedIn = true;
@@ -22,16 +41,29 @@ const checkLoggedIn: RequestHandler = (req, res, next) => {
   next();
 };
 
-const config = {
-  CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRETS: process.env.CLIENT_SECRETS,
-};
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back");
+  }
+);
 
-app.get("/auth/google", (req, res) => {});
-
-app.get("/auth/google/callback", (req, res) => {});
+app.get("/auth/google", passport.authenticate('google', {
+  scope : ['email']
+}));
 
 app.get("/auth/logout", (req, res) => {});
+
+app.get("failure", (req, res) => {
+  return res.status(400).json({
+    error: "Failed to login",
+  });
+});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.json({
